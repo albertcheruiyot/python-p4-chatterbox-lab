58bd49df283da05c6235d45973bdbe1ce240aeb2
Albert Cheruiyot <bettalbert99@gmail.com>
2024-07-08 11:32:42 +0300

    First commit

--- server/app.py
+++ server/app.py
@@ -14,13 +14,50 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        return jsonify([message.to_dict() for message in messages]), 200
+    
+    elif request.method == 'POST':
+        data = request.json
+        new_message = Message(
+            body=data.get('body'),
+            username=data.get('username')
+        )
+        try:
+            db.session.add(new_message)
+            db.session.commit()
+            return jsonify(new_message.to_dict()), 201
+        except IntegrityError:
+            db.session.rollback()
+            return jsonify({"error": "Invalid data"}), 400
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.get(id)
+    if not message:
+        return jsonify({"error": "Message not found"}), 404
+
+    if request.method == 'GET':
+        return jsonify(message.to_dict()), 200
+    
+    elif request.method == 'PATCH':
+        data = request.json
+        for attr in data:
+            setattr(message, attr, data[attr])
+        try:
+            db.session.commit()
+            return jsonify(message.to_dict()), 200
+        except IntegrityError:
+            db.session.rollback()
+            return jsonify({"error": "Invalid data"}), 400
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return '', 204
 
 if __name__ == '__main__':
     app.run(port=5555)
